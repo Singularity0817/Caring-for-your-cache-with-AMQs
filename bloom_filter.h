ced5655fb9d10cc1f6a020b5cb659c9b328b6f3d
jamshed <mdkhan@cs.stonybrook.edu>
2019-11-20 02:25:48 -0500

    Bloom filter library added with serialization support; and modified with slight stuctural changes and a bug correction.

--- bloom_filter.h
+++ bloom_filter.h
@@ -19,12 +19,19 @@ private:
     std::vector<bool> B;    // The bit-array for the bloom filter.
 
 
+
+    void set_parameters(uint64_t numDistinctKeys, double falsePositiveRate);
+
     inline uint64_t get_murmur_hash_slot(uint64_t &key, uint32_t &seed);
+
+    void serialize(std::string &outputFile);
 
 public:
     bloom_filter() {}
 
     bloom_filter(uint64_t numDistinctKeys, double falsePositiveRate);
+
+    bloom_filter(std::string &keyFile, uint64_t numDistinct, double fpr, std::string &outputFile);
 
     void dump_metadata();
 
@@ -35,23 +42,58 @@ public:
 
 
 
-bloom_filter::bloom_filter(uint64_t numDistinctKeys, double falsePositiveRate):
-    n (numDistinctKeys),
-    p (falsePositiveRate)
+void bloom_filter::dump_metadata()
 {
-    B.resize(n);
+    std::clog << "\tn = " << n << ";";
+    std::clog << "\tp = " << p << ";";
+    std::clog << "\tm = " << m << ";";
+    std::clog << "\tk = " << k << ";";
+    std::clog << "\thc = " << hashCombineFactor << ";";
+    std::clog << "\n";
+}
+
+
+
+void bloom_filter::set_parameters(uint64_t numDistinctKeys, double falsePositiveRate)
+{
+    n = numDistinctKeys;
+    p = falsePositiveRate;
 
     m = ceil((-double(n) * log(p)) / (log(2) * log(2)));
     k = (double(m) / n) * log(2);
+
+    B.resize(m + (m % 8 ? (8 - m % 8) : 0));
 
     hashCombineFactor = (((uint64_t(1) << 63) % m) * (2 % m)) % m;
 }
 
 
 
-void bloom_filter::dump_metadata()
+bloom_filter::bloom_filter(uint64_t numDistinctKeys, double falsePositiveRate)
 {
-    std::clog << "n = " << n << "; p = " << p << "; m = " << m << "; hc = " << hashCombineFactor << "\n";
+    set_parameters(numDistinctKeys, falsePositiveRate);
+}
+
+
+
+bloom_filter::bloom_filter(std::string &keyFile, uint64_t numDistinct, double fpr, std::string &outputFile)
+{
+    std::ifstream input(keyFile);
+    if(!input.is_open())
+    {
+        std::cerr << "Cannot open keys file " << keyFile << "\n";
+        exit(1);
+    }
+
+    set_parameters(numDistinct, fpr);
+    dump_metadata();
+
+    uint64_t key;
+    while(input >> key)
+        insert(key);
+
+    
+    serialize(outputFile);
 }
 
 
@@ -82,4 +124,35 @@ bool bloom_filter::query(uint64_t key)
             return false;
 
     return true;
+}
+
+
+
+void bloom_filter::serialize(std::string &outputFile)
+{
+    std::ofstream output;
+    output.open(outputFile.c_str(), std::ios::binary | std::ios::out);
+
+
+    // Serialize the parameters.
+    
+    output.write((const char *)&n, sizeof(n));
+    output.write((const char *)&p, sizeof(p));
+    output.write((const char *)&m, sizeof(m));
+    output.write((const char *)&k, sizeof(k));
+
+
+    // Serialize the bits array.
+
+    for(uint64_t i = 0; i < m; i += 8)
+    {
+        unsigned char byte = 0;
+        for(int j = 0; j < 8; ++j)
+            byte |= B[i + j];
+
+        output.write((const char *)&byte, sizeof(byte));
+    }
+
+
+    output.close();
 }
